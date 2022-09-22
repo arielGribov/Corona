da84671ceb6c87a84aaf46ca0c5caa5b9a9017b2
ariel gribov <argr96@gmail.com>
2022-09-22 17:21:31 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
- 
+const hamburgerButton = document.getElementsByClassName('fa-bars')[0];
+const closingButton = document.getElementsByClassName('fa-xmark')[0];
+const sideBar = document.getElementsByClassName('side-bar')[0];
+
+hamburgerButton.onclick = () => {
+    hamburgerButton.classList.add('display-none');
+    closingButton.classList.remove('display-none');
+    sideBar.classList.remove('display-none');
+};
+closingButton.onclick = () => {
+    hamburgerButton.classList.remove('display-none');
+    closingButton.classList.add('display-none');
+    sideBar.classList.add('display-none');
+};
