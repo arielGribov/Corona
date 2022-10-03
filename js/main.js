c381be6ed69b91eb538bcd348f056e833098c256
ariel gribov <argr96@gmail.com>
2022-10-03 17:08:33 +0300

    main part of css commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const hamburgerButton = document.getElementsByClassName('fa-bars')[0];
 const closingButton = document.getElementsByClassName('fa-xmark')[0];
 const sideBar = document.getElementsByClassName('nav-container')[0];
+const infoSymbol = document.getElementsByClassName('fa-circle-info');
+const infoData = document.getElementsByClassName('info');
 
 hamburgerButton.onclick = () => {
     hamburgerButton.classList.add('display-none');
@@ -12,3 +14,12 @@ closingButton.onclick = () => {
     closingButton.classList.add('display-none');
     sideBar.classList.add('display-none');
 };
+for (let i = 0; i < infoSymbol.length; i++) {
+    infoSymbol[i].onmouseover = () => {
+        infoData[i].classList.remove('display-none');
+        console.log(i);
+    };
+    infoSymbol[i].onmouseout = () => {
+        infoData[i].classList.add('display-none');
+    };
+}
